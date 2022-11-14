ab5131607628536311e951384d91cb22fd238a55
acgnLiu <liu_wenle_acgn@163.com>
2022-11-14 23:49:16 +0800

    debug 20221114

---
+++ auto_demo.py
@@ -0,0 +1,16 @@
+from util.hello_word import HelloWord
+
+class AutoDemo:
+
+    def __init__(self) -> None:
+        pass
+
+    def run_debug(self):
+
+        hw = HelloWord()
+
+
+if __name__ == "__main__":
+
+    auto = AutoDemo()
+    auto.run_debug()
---
+++ util/hello_word.py
@@ -0,0 +1,10 @@
+class HelloWord:
+
+    def __init__(self):
+        
+        self.phrase = "Hello, word!"
+        self.run_main()
+
+    def run_main(self):
+        
+        print(self.phrase)
